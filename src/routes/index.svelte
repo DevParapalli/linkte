dc456c043266ad023e581415d5f7fd976ddfb726
Dev Parapalli <devparapalli@gmail.com>
2021-12-19 22:58:23 +0530

    GTag until i can get some other method. Project Links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 <section class="bg-gray-900 body-font text-gray-400">
 	<div class="mx-auto container pb-10 pt-10 px-5">
 		<div class="text-center mb-20">
-			<h1 class="text-center font-medium mb-4 sm:text-3xl text-2xl text-white title-font">{links_data.title}</h1>
+			<h1 class="text-center font-medium mb-4 sm:text-3xl text-2xl text-green-50 title-font">{links_data.title}</h1>
 			<p class="mx-auto leading-relaxed lg:w-3/4 text-base xl:w-2/4">{links_data.subtitle}</p>
 		</div>
 		<div class="flex flex-wrap m-4">
