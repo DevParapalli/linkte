a3b2fe307e968eb786f208ab8feac2b61e20f9f1
Dev Parapalli <devparapalli@gmail.com>
2021-12-21 15:58:03 +0530

    Updated Styles before the sm point.

--- src/lib/LinkSection.svelte
+++ src/lib/LinkSection.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <div class="lg:w-1/4 p-4 sm:w-1/2 w-full">
-	<h2 title="{link_section_data.description}" class="sm:text-left text-center font-medium mb-4 text-sm text-white title-font tracking-widest">{link_section_data.title}</h2>
-	<nav class="items-center -mb-1 flex flex-col sm:items-start sm:text-left space-y-2 text-center">
+	<h2 title="{link_section_data.description}" class="w-full bg-gray-800 p-2 rounded sm:bg-gray-900 sm:text-left text-center font-medium mb-4 text-sm text-white title-font tracking-widest">{link_section_data.title}</h2>
+	<nav class="items-center -mb-1 flex flex-col sm:items-start text-left space-y-2">
         {#each link_section_data.links as link,i}
             <LinkItem link_data={link}/>
         {/each}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex flex-col h-screen justify-between bg-gray-900">
-	<div class="mb-auto flex-grow overflow-y-auto"><slot /></div>
-	<footer class="text-gray-400 bg-gray-900 body-font shadow-lg opacity-30 hover:opacity-100 transition-opacity duration-500">
+	<div class="mb-auto flex-grow md:overflow-y-auto"><slot /></div>
+	<footer class="text-gray-400 bg-gray-900 body-font md:opacity-30 hover:opacity-100 transition-opacity duration-500">
 		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 		  <a class="flex title-font font-medium items-center md:justify-start justify-center text-white" href="/">
 			<svg class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M10.59 13.41c.41.39.41 1.03 0 1.42c-.39.39-1.03.39-1.42 0a5.003 5.003 0 0 1 0-7.07l3.54-3.54a5.003 5.003 0 0 1 7.07 0a5.003 5.003 0 0 1 0 7.07l-1.49 1.49c.01-.82-.12-1.64-.4-2.42l.47-.48a2.982 2.982 0 0 0 0-4.24a2.982 2.982 0 0 0-4.24 0l-3.53 3.53a2.982 2.982 0 0 0 0 4.24m2.82-4.24c.39-.39 1.03-.39 1.42 0a5.003 5.003 0 0 1 0 7.07l-3.54 3.54a5.003 5.003 0 0 1-7.07 0a5.003 5.003 0 0 1 0-7.07l1.49-1.49c-.01.82.12 1.64.4 2.43l-.47.47a2.982 2.982 0 0 0 0 4.24a2.982 2.982 0 0 0 4.24 0l3.53-3.53a2.982 2.982 0 0 0 0-4.24a.973.973 0 0 1 0-1.42z" fill="currentColor"/></svg>
