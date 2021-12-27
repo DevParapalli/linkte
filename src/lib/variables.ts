8d96a99774b4a79d180a318d10e2af008cd56465
Dev Parapalli <devparapalli@gmail.com>
2021-12-27 12:43:35 +0530

    Removed error with undefined variables

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,7 +1,7 @@
 export const variables = {
 	// Do we really need a db?
-	FAUNADB_SECRET: import.meta.env.VITE_FAUNADB_SECRET.toString(),
-	FAUNADB_DOMAIN: import.meta.env.VITE_FAUNADB_DOMAIN.toString(),
+	//FAUNADB_SECRET: import.meta.env.VITE_FAUNADB_SECRET.toString(),
+	//FAUNADB_DOMAIN: import.meta.env.VITE_FAUNADB_DOMAIN.toString(),
 	// DweetAPI Key.
 	DWEET_KEY: import.meta.env.VITE_DWEET_KEY.toString(),
 	TINYMCE_KEY: import.meta.env.VITE_TINYMCE_KEY.toString()
