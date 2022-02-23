d83c77c437516fa9b50b54241af54969f1bd986c
Dev Parapalli <devparapalli@gmail.com>
2022-02-23 22:58:48 +0530

    Added Class Content

---
+++ src/routes/redir/ar.svelte
@@ -0,0 +1,10 @@
+<script>
+import { goto } from "$app/navigation";
+
+import { onMount } from "svelte";
+
+
+    onMount(() => {
+        goto('https://ar-js-org.github.io/AR.js/aframe/examples/marker-based/basic.html')
+    })
+</script>
