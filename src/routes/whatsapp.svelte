6ffafcf15bbe5a5372003c86edd83dabbf59655a
Dev Parapalli <devparapalli@gmail.com>
2022-03-17 13:16:17 +0530

    updated deps, added enter key submission

--- src/routes/whatsapp.svelte
+++ src/routes/whatsapp.svelte
@@ -22,6 +22,12 @@ import { goto } from "$app/navigation";
         dev ? console.log(event): null;
         goto(`https://wa.me/${get_formatted_number(number_input)}`);
     }
+    function on_enter_key(event) {
+        dev ? console.log(event): null;
+        if (event.key === "Enter") {
+            on_submit(event);
+        }
+    }
 </script>
 
 <section class="text-gray-400 bg-gray-900 body-font">
@@ -33,7 +39,7 @@ import { goto } from "$app/navigation";
         <div class="flex w-full justify-center items-end">
           <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
             <label for="hero-field" class="leading-7 text-sm text-gray-400">Enter phone number (Intl or 10-digit formats)</label>
-            <input type="text" bind:value="{number_input}" pattern="\d+" id="hero-field" name="hero-field" class="w-full bg-gray-800 rounded border bg-opacity-40 border-gray-700 focus:ring-2 focus:ring-green-900 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            <input on:keypress="{on_enter_key}" type="text" bind:value="{number_input}" pattern="\d+" id="hero-field" name="hero-field" class="w-full bg-gray-800 rounded border bg-opacity-40 border-gray-700 focus:ring-2 focus:ring-green-900 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
           <button on:click="{on_submit}" class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
         </div>
